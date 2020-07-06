0185bba1406ead204840e624cd96379d047d54aa
hardcoding <sonlcr1@naver.com>
2020-07-06 12:41:12 +0900

    hotfix frgment second viewpager

--- app/src/main/java/com/sonlcr1/projectrecipe/MainActivity.java
+++ app/src/main/java/com/sonlcr1/projectrecipe/MainActivity.java
@@ -6,11 +6,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    //MyAdapter adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         
-        //hotfix 수정부분
+
     }
 }
