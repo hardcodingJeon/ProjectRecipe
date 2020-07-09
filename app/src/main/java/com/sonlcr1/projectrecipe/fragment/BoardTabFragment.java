1abd170f9c6415d07d073dcf396b8565a290534e
hardcoding <sonlcr1@naver.com>
2020-07-09 16:44:20 +0900

    dothome board activity

--- app/src/main/java/com/sonlcr1/projectrecipe/fragment/BoardTabFragment.java
+++ app/src/main/java/com/sonlcr1/projectrecipe/fragment/BoardTabFragment.java
@@ -17,6 +17,7 @@ public class BoardTabFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_boardtab,container,false);
 
+        //todo : board recycler 시안 만들차례
 
         return view;
     }
