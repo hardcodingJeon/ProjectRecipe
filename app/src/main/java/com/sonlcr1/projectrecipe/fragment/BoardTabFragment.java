bd416012be366e72f2e2247629352c5d8e59b768
hardcoding <sonlcr1@naver.com>
2020-07-24 14:38:01 +0900

    first

--- app/src/main/java/com/sonlcr1/projectrecipe/fragment/BoardTabFragment.java
+++ app/src/main/java/com/sonlcr1/projectrecipe/fragment/BoardTabFragment.java
@@ -19,7 +19,7 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import com.bumptech.glide.Glide;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.sonlcr1.projectrecipe.BoardEdit;
+import com.sonlcr1.projectrecipe.recipeActivity.BoardEdit;
 import com.sonlcr1.projectrecipe.R;
 import com.sonlcr1.projectrecipe.RetrofitHelper;
 import com.sonlcr1.projectrecipe.RetrofitService;
@@ -35,12 +35,8 @@ import retrofit2.Retrofit;
 
 public class BoardTabFragment extends Fragment {
 
-    String imgUrl = "http://jeondh9971.dothome.co.kr/Recipe/recipeBoardEdit/";
 
     ArrayList<Board> datas = new ArrayList<>();
-
-    TextView tv;
-    ImageView iv;
 
     RecyclerView recyclerView;
     BoardAdapter boardAdapter;
@@ -58,25 +54,8 @@ public class BoardTabFragment extends Fragment {
         fab = view.findViewById(R.id.fabBtn);
         refreshLayout = view.findViewById(R.id.refresh);
 
-        //tv = view.findViewById(R.id.tv);
-        //iv = view.findViewById(R.id.iv);
-
         recyclerView = view.findViewById(R.id.recycle);
         context = getContext();
-
-//        datas.add(new Board(null,"1","1","msg1",null,0,1,"1"));
-//        datas.add(new Board(null,"2","2","msg2",null,0,2,"2"));
-//        datas.add(new Board(null,"1","1","msg1",null,0,1,"1"));
-//        datas.add(new Board(null,"2","2","msg2",null,0,2,"2"));
-//        datas.add(new Board(null,"1","1","msg1",null,0,1,"1"));
-//        datas.add(new Board(null,"2","2","msg2",null,0,2,"2"));
-//        datas.add(new Board(null,"1","1","msg1",null,0,1,"1"));
-//        datas.add(new Board(null,"2","2","msg2",null,0,2,"2"));
-
-        // 서버측 트래픽 문제로 일단 주석 처리
-        //activity();
-
-
 
         return view;
     }
@@ -97,51 +76,9 @@ public class BoardTabFragment extends Fragment {
             @Override
             public void onRefresh() {
                 datas.removeAll(datas);
-                //activity();
                 refreshLayout.setRefreshing(false);
             }
         });
     }
 
-    void activity(){
-        Retrofit retrofit = RetrofitHelper.getRetrofitInstance();
-        RetrofitService retrofitService = retrofit.create(RetrofitService.class);
-        Call<ArrayList<Board>> call = retrofitService.loadData();
-        call.enqueue(new Callback<ArrayList<Board>>() {
-            @Override
-            public void onResponse(Call<ArrayList<Board>> call, Response<ArrayList<Board>> response) {
-                if (response.isSuccessful()) {
-                    ArrayList<Board> items = response.body();
-                    for (Board e : items) {
-
-                        datas.add(0, e);
-
-                        boardAdapter = new BoardAdapter(context, datas);
-                        boardAdapter.notifyDataSetChanged();
-
-                        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(context, RecyclerView.VERTICAL, false);
-                        recyclerView.setLayoutManager(layoutManager);
-                        recyclerView.setAdapter(boardAdapter);
-                    }
-                }
-            }
-
-            @Override
-            public void onFailure(Call<ArrayList<Board>> call, Throwable t) {
-
-            }
-        });
-
-//        TextView tv = view.findViewById(R.id.tv);
-//        ImageView iv = view.findViewById(R.id.iv);
-//        tv.setText(datas.get(0).msg);
-//        Glide.with(context).load(imgUrl+datas.get(0).imgmain).into(iv);
-//
-//        boardAdapter = new BoardAdapter(context,datas);
-//        boardAdapter.notifyDataSetChanged();
-//
-//        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(context,RecyclerView.VERTICAL,false);
-//        recyclerView.setLayoutManager(layoutManager);
-//        recyclerView.setAdapter(boardAdapter);
-    }
 }
